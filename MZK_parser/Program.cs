c4e5af02289a1fda0907f84e9874557e407a0b4a
janfri <janfri@wp.pl>
2017-01-08 23:23:23 +0100

    parsing times

--- MZK_parser/Program.cs
+++ MZK_parser/Program.cs
@@ -3,6 +3,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -33,8 +34,68 @@ namespace MZK_parser
         public string busNo;
     }
 
+    public class BusStop
+    {
+        public List<BusStopDetail> busStopDetail;
+    }
+    public class BusStopDetail
+    {
+        public string stopRef;
+        public string name;
+        public List<TimeTable> timeTable;
+    }
+
+    public class TimeTable
+    {
+        public string line;
+        public List<Time> time;
+    }
+
+    public class Time
+    {
+        public string day;
+        public List<Hour> hour;
+    }
+
+    public class Hour
+    {
+        public string minutes;
+    }
+
+
     class Program
     {
+
+
+        static void Main(string[] args)
+        {
+            //html agile config
+            HtmlWeb htmlWeb = new HtmlWeb()
+            {
+                AutoDetectEncoding = false,
+                OverrideEncoding = Encoding.GetEncoding("iso-8859-2")
+            };
+            //read html page
+            HtmlDocument htmlDocument = htmlWeb.Load("http://www.mzkb-b.internetdsl.pl/linie_r.htm#1");
+            IEnumerable<HtmlNode> table = htmlDocument.DocumentNode.SelectNodes("//table").Descendants("tr");
+
+            List<BusLink> busLinkList = new List<BusLink>();
+
+            //extract content from main page
+            // MainPageExtract(table, busLinkList);                       
+
+            List<BusStopLink> busStopLink = new List<BusStopLink>();
+            List<string> processedLinks = new List<string>();
+
+            // ExtractLinks(busStopLink, busLinkList, htmlWeb, processedLinks);      
+
+            GetTimeTable();
+
+
+        }
+
+
+
         private static void MainPageExtract(IEnumerable<HtmlNode> table, List<BusLink> busLinkList)
         {
             foreach (var item in table)
@@ -97,8 +158,6 @@ namespace MZK_parser
             JArray busLinks = (JArray)JToken.FromObject(busLinkList);
             System.IO.File.WriteAllText("buss4.json", busLinks.ToString());
         }
-
-
         private static void ExtractLinks(List<BusStopLink> busStopLink, List<BusLink> busLinkList, HtmlWeb htmlWeb, List<string> processsedLinks)
         {
             foreach (var bLink in busLinkList)
@@ -111,7 +170,7 @@ namespace MZK_parser
 
                 var tab = doc.DocumentNode.SelectNodes("//table").Descendants("tr");
 
-                
+
 
                 foreach (var stop in tab)
                 {
@@ -166,7 +225,6 @@ namespace MZK_parser
             JArray stops = (JArray)JToken.FromObject(busStopLink);
             System.IO.File.WriteAllText("stops7.json", stops.ToString());
         }
-
         private static List<BusNo2Stop> ExtractTimeTableLinks(string extractedStopLink)
         {
             HtmlWeb htmlWeb = new HtmlWeb();
@@ -182,40 +240,119 @@ namespace MZK_parser
                     busNo = link.InnerText,
                     link = link.Attributes["href"].Value
                 });
-                
+
             }
             //Console.WriteLine(extractedStopLink);
             return timeTableLinks;
 
         }
-
-        static void Main(string[] args)
+        private static void GetTimeTable()
         {
-            //html agile config
+
+            JArray busStopArray;
+
+            using (StreamReader file = File.OpenText("stops7.json"))
+            using (JsonTextReader reader = new JsonTextReader(file))
+            {
+                busStopArray = (JArray)JToken.ReadFrom(reader);
+
+            }
+
+            BusStop busStop = new BusStop();
+            busStop.busStopDetail = new List<BusStopDetail>();
+
+            foreach (var stop in busStopArray)
+            {
+
+                List<TimeTable> tempTimeTableList = new List<TimeTable>();
+                foreach (var tt in stop["timeTableLink"])
+                {
+                    GetTimeTableHours((string)tt["link"]);
+
+                    tempTimeTableList.Add(new TimeTable
+                    {
+                        line = (string)tt["busNo"]
+                    });
+                }
+
+                BusStopDetail tempBusStopDetail = new BusStopDetail
+                {
+                    name = (string)stop["stopName"],
+                    stopRef = (string)stop["stopRef"],
+                    timeTable = tempTimeTableList
+                };
+
+                busStop.busStopDetail.Add(tempBusStopDetail);
+
+                var n = busStop.busStopDetail;
+            }
+
+            JObject busStops = (JObject)JToken.FromObject(busStop);
+            System.IO.File.WriteAllText("timeTable.json", busStops.ToString());
+
+        }
+
+        private static void GetTimeTableHours(string link2TimeTable)
+        {
+            string link = "http://www.mzkb-b.internetdsl.pl/" + link2TimeTable;
             HtmlWeb htmlWeb = new HtmlWeb()
             {
                 AutoDetectEncoding = false,
                 OverrideEncoding = Encoding.GetEncoding("iso-8859-2")
             };
-            //read html page
-            HtmlDocument htmlDocument = htmlWeb.Load("http://www.mzkb-b.internetdsl.pl/linie_r.htm#1");
-            IEnumerable<HtmlNode> table = htmlDocument.DocumentNode.SelectNodes("//table").Descendants("tr");
 
-            List<BusLink> busLinkList = new List<BusLink>();
-            
-            //extract content from main page
-            MainPageExtract(table, busLinkList);                       
+            HtmlDocument doc = htmlWeb.Load(link);
 
-            List<BusStopLink> busStopLink = new List<BusStopLink>();
-            List<string> processedLinks = new List<string>();
+            var table = doc.DocumentNode.SelectNodes("//table").Descendants("tr");
 
-            ExtractLinks(busStopLink, busLinkList, htmlWeb, processedLinks);       
+            foreach (var item in table)
+            {
+                if (item.InnerText.Contains("DNI ROBOCZE") || item.InnerText.Contains("SOBOTY") || item.InnerText.Contains("NIEDZIELE I ŚWIĘTA"))
+                {
+                    var rows = item.ChildNodes;
+
+                    int rowNo = 0;
+                    List<string> hours = new List<string>();
+                    foreach (var row in rows)
+                    {
+                        if (row.InnerText == "&nbsp;\n")
+                        {
+                            rowNo++;
+                            continue;
+                        }
+
+                        bool nextHour = false;
+                        var values = row.ChildNodes;
+
+                        foreach (var value in values)
+                        {
+                            if (value.InnerText.Length >= 2)
+                            {
+                                int minutes;
+                                string hour = value.InnerText.Substring(0, 2);
+                                if (int.TryParse(hour, out minutes))
+                                {
+                                    hours.Add(rowNo + "."  + minutes.ToString());
+                                    nextHour = true;
+                                }
+                            }
+                        }
+                        if (nextHour)
+                        {
+                            rowNo++;
+                            nextHour = false;
+                        }
+                        
+                    }                 
+                }
+            }
         }
 
 
     }
-
 }
+
+
 
 
 
