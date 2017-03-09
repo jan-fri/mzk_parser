463b61101f9a102984a2c7b78c57fd77fe0c5662
janfri <janfri@wp.pl>
2017-03-09 19:26:09 +0100

    CSV load to Neo4j

--- MZK_parser/Program.cs
+++ MZK_parser/Program.cs
@@ -97,8 +97,17 @@ namespace MZK_parser
         //extracts bus stop list for each bus line number - used in graph database
         private static void GetBusStopList(List<BusStopLink> busStopLink, List<BusLink> busLinkList, HtmlWeb htmlWeb, List<string> processedLinks)
         {
+            List<string> processedStops = new List<string>();
+            Dictionary<string, List<string>> processedRelations = new Dictionary<string, List<string>>();
 
-            System.IO.StreamWriter file = new System.IO.StreamWriter("graphDatabaseScript.txt");
+            System.IO.StreamWriter busStopsFile = new System.IO.StreamWriter("busStops.csv");
+            System.IO.StreamWriter createRelationsFile = new System.IO.StreamWriter("createRelations.csv");
+            System.IO.StreamWriter mergeRelationsFile = new System.IO.StreamWriter("mergeRelations.csv");
+
+            busStopsFile.WriteLine("ref,name");
+            createRelationsFile.WriteLine("busA,line,busB");
+            mergeRelationsFile.WriteLine("busA,line,busB");
+
             foreach (var bLink in busLinkList)
             {
                 if (bLink.busNumber == null)
@@ -110,7 +119,7 @@ namespace MZK_parser
                 var tab = doc.DocumentNode.SelectNodes("//table").Descendants("tr");
 
 
-                
+
 
                 foreach (var stop in tab)
                 {
@@ -132,19 +141,12 @@ namespace MZK_parser
                                 stopName = stopName
                             };
 
-
-
                             if (processedLinks.Contains(extractedStopLink))
-                            {
                                 break;
-                            }
                             else
-                            {
                                 processedLinks.Add(extractedStopLink);
-                            }
 
-                            busStopLink.Add(tempStopLink);                           
-
+                            busStopLink.Add(tempStopLink);
                         }
                     }
                 }
@@ -152,58 +154,142 @@ namespace MZK_parser
                 processedLinks.Clear();
 
                 int index = 0;
-                List<string> nodeList = new List<string>();
-                List<string> createRelationList = new List<string>();
-                List<string> refList = new List<string>();
-                StringBuilder busStopList = new StringBuilder();
+                StringBuilder busStopBuilder = new StringBuilder();
+                StringBuilder createRelationsBuilder = new StringBuilder();
+                StringBuilder mergeRelationsBuilder = new StringBuilder();
+
                 foreach (var busStop in busStopLink)
                 {
-                    nodeList.Add("MERGE (b" + busStop.stopRef + ":BusStop {name:'" + busStop.stopName + "', ref:'" + busStop.stopRef + "'})");
+                    if (!processedStops.Contains(busStop.stopRef))
+                    {
+                        busStopBuilder.Append(busStop.stopRef + "," + busStop.stopName + "\n");
+                        processedStops.Add(busStop.stopRef);
+                    }
 
                     if (index < busStopLink.Count - 1)
                     {
-                        createRelationList.Add("OPTIONAL MATCH p=(b" + busStop.stopRef + ":BusStop {ref:'" + busStop.stopRef + "'})-[r:RELATION]->(b" + busStopLink[index + 1].stopRef + ":BusStop {ref:'" + busStopLink[index + 1].stopRef + "'})\n" +
-                           "FOREACH (c IN CASE WHEN p IS NULL THEN [1] ELSE [] END |" +
-                           "CREATE (b" + busStop.stopRef + ")-[r:RELATION {busNumber:" + bLink.busNumber + "}]->(b" + busStopLink[index + 1].stopRef + ")\n" +
-                           ")\n" +
-                           "FOREACH (c IN CASE WHEN p IS NOT NULL THEN [1] ELSE [] END |" +
-                           "MERGE (b" + busStop.stopRef + ")-[r:RELATION]->(b" + busStopLink[index + 1].stopRef + ") SET r.busNumber = r.busNumber + [" + bLink.busNumber + "]\n" +
-                           ")\n");
+                        bool relationCreated = false;
+                        bool relationExists = false;
+
+                        if (processedRelations.ContainsKey(busStop.stopRef))
+                        {
+                            relationExists = true;
+                            foreach (var rel in processedRelations[busStop.stopRef])
+                            {
+                                if (rel == busStopLink[index + 1].stopRef)
+                                {
+                                    mergeRelationsBuilder.Append(busStop.stopRef + "," + bLink.busNumber + "," + busStopLink[index + 1].stopRef + "\n");
+                                    relationCreated = true;
+                                    break;
+                                }
+                            }
+                            processedRelations[busStop.stopRef].Add(busStopLink[index + 1].stopRef);                            
+                        }
+
+                        if (!relationCreated)
+                        {
+                            createRelationsBuilder.Append(busStop.stopRef + "," + bLink.busNumber + "," + busStopLink[index + 1].stopRef + "\n");
+                            if (!relationExists)
+                            {
+                                processedRelations.Add(busStop.stopRef, new List<string>() { busStopLink[index + 1].stopRef });
+                            }
+                        }
                     }
-
-                    refList.Add("b" + busStop.stopRef);
-
-                    busStopList.Append("b" + busStop.stopRef + ", ");
-
                     index++;
                 }
 
                 busStopLink.Clear();
 
-                file.WriteLine("// create nodes for stops for line " + bLink.busNumber + " route " + bLink.direction);
-                foreach (var node in nodeList)
-                {
-                    file.WriteLine(node);
-                }
-                file.WriteLine();
-                file.WriteLine("// line " + bLink.busNumber + " route " + bLink.direction + ":" + busStopList);
-
-                busStopList.Remove(busStopList.Length - 2, 2);
-
-
-                for (int i = 0; i < createRelationList.Count; i++)
-                {
-                    file.WriteLine("WITH " + busStopList);                
-                    file.WriteLine(createRelationList[i]);
-                }
-
-                file.WriteLine();
-               
+                busStopsFile.WriteLine(busStopBuilder);
+                createRelationsFile.WriteLine(createRelationsBuilder);
+                mergeRelationsFile.WriteLine(mergeRelationsBuilder);
             }
-            file.Close();
-
+            busStopsFile.Close();
+            createRelationsFile.Close();
+            mergeRelationsFile.Close();
         }
 
+        //private static void GetBusStopList(List<BusStopLink> busStopLink, List<BusLink> busLinkList, HtmlWeb htmlWeb, List<string> processedLinks)
+        //{
+        //    List<string> processedStops = new List<string>();
+
+        //    System.IO.StreamWriter busStopsFile = new System.IO.StreamWriter("busStops.csv");
+        //    System.IO.StreamWriter relationsFile = new System.IO.StreamWriter("relations.csv");
+
+        //    busStopsFile.WriteLine("ref,name");
+        //    relationsFile.WriteLine("busA,line,busB");
+
+        //    foreach (var bLink in busLinkList)
+        //    {
+        //        if (bLink.busNumber == null)
+        //            continue; // break;
+
+        //        string link = "http://www.mzkb-b.internetdsl.pl/" + bLink.linktoBus;
+        //        HtmlDocument doc = htmlWeb.Load(link);
+
+        //        var tab = doc.DocumentNode.SelectNodes("//table").Descendants("tr");
+
+        //        foreach (var stop in tab)
+        //        {
+        //            IEnumerable<HtmlNode> stoplinks = stop.Descendants("a").Where(x => x.Attributes.Contains("href"));
+        //            foreach (var sLink in stoplinks)
+        //            {
+        //                BusStopLink tempStopLink;
+        //                if (sLink.InnerText.Contains('('))
+        //                {
+        //                    string stopName = Regex.Replace(sLink.InnerText.Replace("&nbsp;", ""), "(\\(.*\\))", "");
+
+        //                    string stopNo = sLink.Attributes["href"].Value.Split('_', '_')[1];
+        //                    string extractedStopLink = "p_" + stopNo + "_l.htm";
+
+        //                    tempStopLink = new BusStopLink
+        //                    {
+        //                        stopRef = sLink.InnerText.Split('(', ')')[1],
+        //                        stopLink = extractedStopLink,
+        //                        stopName = stopName
+        //                    };
+
+        //                    if (processedLinks.Contains(extractedStopLink))
+        //                        break;
+        //                    else
+        //                        processedLinks.Add(extractedStopLink);
+
+        //                    busStopLink.Add(tempStopLink);
+        //                }
+        //            }
+        //        }
+
+        //        processedLinks.Clear();
+
+        //        int index = 0;
+        //        StringBuilder busStopBuilder = new StringBuilder();
+        //        StringBuilder relationsBuilder = new StringBuilder();
+
+        //        foreach (var busStop in busStopLink)
+        //        {
+        //            if (!processedStops.Contains(busStop.stopRef))
+        //            {
+        //                busStopBuilder.Append(busStop.stopRef + "," + busStop.stopName + "\n");
+        //                processedStops.Add(busStop.stopRef);
+        //            }
+
+        //            if (index < busStopLink.Count - 1)
+        //            {
+        //                relationsBuilder.Append(busStop.stopRef + "," + bLink.busNumber + "," + busStopLink[index + 1].stopRef + "\n");
+        //            }
+
+        //            index++;
+        //        }
+
+        //        busStopLink.Clear();
+
+        //        busStopsFile.WriteLine(busStopBuilder);
+        //        relationsFile.WriteLine(relationsBuilder);
+        //    }
+        //    busStopsFile.Close();
+        //    relationsFile.Close();
+        //}
+        
 
 
         //extracts bus line numbers, corresponding links to bus stops and route direction 
